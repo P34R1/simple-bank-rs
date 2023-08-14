18f69df417af5da32c0007881cbc4bf81446479d
P34RL <undeadgamer279@gmail.com>
2023-08-14 01:50:55 -0400

    Added account struct base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+pub struct Account {
+    balance: f64,
+}
+
+impl Account {
+    pub fn new(balance: f64) -> Self {
+        Account { balance }
+    }
+
+    pub fn get_balance(&self) -> f64 {
+        self.balance
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let account1 = Account::new(1000.0);
+    let account2 = Account::new(500.0);
+
+    println!("Account 1 Balance: {}", account1.get_balance());
+    println!("Account 2 Balance: {}", account2.get_balance());
 }
